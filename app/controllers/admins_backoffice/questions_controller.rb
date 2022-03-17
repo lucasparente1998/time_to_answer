3c0e4cde4bef1baccfb56598ed948099c8455d60
Lucas Parente <lucasparente@id.uff.br>
2022-03-17 15:50:11 -0300

    Good practice MVC

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -2,7 +2,7 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   before_action :set_question, only: [:edit, :update, :destroy]
   before_action :get_subjects, only: [:new, :edit]
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.includes(:subject).order(:description).page(params[:page])
   end
 
   def new
