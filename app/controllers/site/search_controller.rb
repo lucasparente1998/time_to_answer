ace7464d65e6199725a3ad736a9a4360ae669b27
Lucas Parente <lucasparente@id.uff.br>
2022-03-21 23:03:00 -0300

    search term message

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,5 +1,7 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.includes(:answers).page(params[:page])
+    # params[:term]
+    # @questions = Question.includes(:answers).page(params[:page])
+    @questions = Question._search_(params[:page], params[:term])
   end
 end
